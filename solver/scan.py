9b6716cfda126cfc14da2097cd09cb3d1c3f1533
Josef Gugglberger <josef.gugglberger@student.uibk.ac.at>
2018-08-03 14:53:01 +0200

    adapt to support pictures with grids in it

--- solver/scan.py
+++ solver/scan.py
@@ -11,19 +11,23 @@ model.train(samples, cv2.ml.ROW_SAMPLE, responses)
 
 # testing part
 
-im = cv2.imread('test.png')
+im = cv2.imread('testsudoku.png')
+out = np.zeros(im.shape, np.uint8)
 gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 thresh = cv2.adaptiveThreshold(gray, 255, 1, 1, 11, 2)
-
 
 _, contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
 field = []
 row = []
+maxi = 0
 for cnt in contours:
-    if cv2.contourArea(cnt) > 40:
+    if 1000 > cv2.contourArea(cnt) > 40:
+        if cv2.contourArea(cnt) > maxi:
+            maxi = cv2.contourArea(cnt)
         [x, y, w, h] = cv2.boundingRect(cnt)
         if h > 28:
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 2)
             roi = thresh[y:y + h, x:x + w]
             roismall = cv2.resize(roi, (10, 10))
             roismall = roismall.reshape((1, 100))
@@ -31,6 +35,7 @@ for cnt in contours:
             retval, results, neigh_resp, dists = model.findNearest(roismall, k=1)
             string = str(int((results[0][0])))
             row.append([string, x, y])
+            cv2.putText(out, string, (x, y + h), 0, 1, (0, 255, 0))
 
         if len(row) == 25:
             # sort by x coordinate
@@ -41,9 +46,14 @@ for cnt in contours:
 # reverse order
 field = field[::-1]
 
+print maxi
+
 with open("file.dat", "w+") as f:
     for i in range(0, len(field)):
         for j in range(0, len(field[0])):
             f.write(str(field[i][j][0]) + ' ')
 
         f.write("\n")
+
+cv2.imshow('out', out)
+cv2.waitKey(0)
