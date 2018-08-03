c985a2b6f6c1d12c92348ec86e1e40871e1ed248
Josef Gugglberger <josef.gugglberger@student.uibk.ac.at>
2018-08-03 14:40:15 +0200

    change export to text file instead of png

--- solver/scan.py
+++ solver/scan.py
@@ -12,30 +12,38 @@ model.train(samples, cv2.ml.ROW_SAMPLE, responses)
 # testing part
 
 im = cv2.imread('test.png')
-out = np.zeros(im.shape, np.uint8)
 gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 thresh = cv2.adaptiveThreshold(gray, 255, 1, 1, 11, 2)
 
+
 _, contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-result = np.empty(shape=[])
-
-# TODO order contours from left to right, rows are sorted by default
+field = []
+row = []
 for cnt in contours:
-    if cv2.contourArea(cnt) > 50:
+    if cv2.contourArea(cnt) > 40:
         [x, y, w, h] = cv2.boundingRect(cnt)
         if h > 28:
-            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 2)
             roi = thresh[y:y + h, x:x + w]
             roismall = cv2.resize(roi, (10, 10))
             roismall = roismall.reshape((1, 100))
             roismall = np.float32(roismall)
             retval, results, neigh_resp, dists = model.findNearest(roismall, k=1)
             string = str(int((results[0][0])))
-            print string, x, y, h
-            # TODO write to an result array or file
-            cv2.putText(out, string, (x, y + h), 0, 1, (0, 255, 0))
+            row.append([string, x, y])
 
-cv2.imshow('im', im)
-cv2.imshow('out', out)
-cv2.waitKey(0)
+        if len(row) == 25:
+            # sort by x coordinate
+            row.sort(key=lambda x: int(x[1]))
+            field.append(row)
+            row = []
+
+# reverse order
+field = field[::-1]
+
+with open("file.dat", "w+") as f:
+    for i in range(0, len(field)):
+        for j in range(0, len(field[0])):
+            f.write(str(field[i][j][0]) + ' ')
+
+        f.write("\n")
