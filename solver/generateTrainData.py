07cde925c34f60b7909de22f109e558685e5a24f
Josef Gugglberger <josef.gugglberger@student.uibk.ac.at>
2018-08-04 16:36:11 +0200

    fix bug in generateTrainData.py and adapt parameters in findContours, works now with sudokus of size 430x430

--- solver/generateTrainData.py
+++ solver/generateTrainData.py
@@ -6,9 +6,14 @@ import sys
 import numpy as np
 import cv2
 
+# images should be sized roughly to 400x400
+
 if len(sys.argv) < 2:
     print "usage: python generateTrainData.py {trainImage1, trainImage2, ...}"
     exit(-1)
+
+responses = []
+samples = np.empty((0, 100))
 
 for i in range(1, len(sys.argv)):
     im = cv2.imread(sys.argv[i])
@@ -16,22 +21,18 @@ for i in range(1, len(sys.argv)):
 
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
+    thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 5, 2)
 
     # finding Contours
 
     _, contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-    samples = np.empty((0, 100))
-    responses = []
     keys = [i for i in range(48, 58)]
 
     for cnt in contours:
-        if 2500 > cv2.contourArea(cnt) > 50:
-            print cv2.contourArea(cnt)
+        if 2500 > cv2.contourArea(cnt) > 130:
             [x, y, w, h] = cv2.boundingRect(cnt)
-
-            if h > 28:
+            if h > 30:
                 cv2.rectangle(im, (x, y), (x + w, y + h), (0, 0, 255), 2)
                 roi = thresh[y:y + h, x:x + w]
                 roismall = cv2.resize(roi, (10, 10))
@@ -47,8 +48,8 @@ for i in range(1, len(sys.argv)):
                     sample = roismall.reshape((1, 100))
                     samples = np.append(samples, sample, 0)
 
-    responses = np.array(responses, np.float32)
-    responses = responses.reshape((responses.size, 1))
+responses = np.array(responses, np.float32)
+responses = responses.reshape((responses.size, 1))
 
 
 print "training complete"
--- solver/scan.py
+++ solver/scan.py
@@ -20,24 +20,28 @@ if len(sys.argv) < 2:
 im = cv2.imread(sys.argv[1])
 out = np.zeros(im.shape, np.uint8)
 gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-thresh = cv2.adaptiveThreshold(gray, 255, 1, 1, 11, 2)
+thresh = cv2.adaptiveThreshold(gray, 255, 1, 1, 5, 2)
 
 _, contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
 row = []
 field = []
 for cnt in contours:
-    if 2500 > cv2.contourArea(cnt) > 50:
+    if 2500 > cv2.contourArea(cnt) > 150:
         [x, y, w, h] = cv2.boundingRect(cnt)
-        if h > 28:
-            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        if h > 35:
+            # k-nearest
             roi = thresh[y:y + h, x:x + w]
             roismall = cv2.resize(roi, (10, 10))
             roismall = roismall.reshape((1, 100))
             roismall = np.float32(roismall)
-            retval, results, neigh_resp, dists = model.findNearest(roismall, k=1)
+            _, results, _, _ = model.findNearest(roismall, k=1)
             string = str(int((results[0][0])))
             row.append([string, x, y])
+            # print len(row)
+
+            # write to output image
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 2)
             cv2.putText(out, string, (x, y + h), 0, 1, (0, 255, 0))
 
         if len(row) == 9:
@@ -54,6 +58,8 @@ with open(sys.argv[1] + '.txt', "w+") as f:
         for j in range(0, len(field[0])):
             f.write(str(field[i][j][0]) + ' ')
         f.write("\n")
+
+print 'finished '
 
 cv2.imshow('out', out)
 cv2.waitKey(0)
