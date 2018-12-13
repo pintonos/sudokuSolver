7f0c8cadc6baf51754526d151949f31a43d6f647
Josef Gugglberger <j.gugglberger@gmail.com>
2018-12-13 13:29:17 +0100

    add image generation for solution

--- solver/solver.py
+++ solver/solver.py
@@ -35,50 +35,51 @@ def solve(A):
         bs = [vars[3 * br + k][3 * bc + l] for k in range(3) for l in range(3)]
         solver.add(all9(bs))
 
+    A = []
     result = solver.check()  # check for satisfiability
     if result == z3.sat:
         model = solver.model()  # get valuation
         for row in vars:
-            print(reduce(lambda s, v: s + str(model[v]) + " ", row, ""))
-    else:
-        print("no solution found")
+            s = reduce(lambda s, v: s + str(model[v]) + " ", row, "")
+            A.append(s)
+    return A
 
 
-# main
+def generate_image(A):
+    image = np.zeros((210, 210, 4), np.uint8)
+    for i, row in enumerate(A, 1):
+        cv2.putText(image, row, (25, 22 * i), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255, 255))
+    cv2.imshow("Result", image)
+    cv2.waitKey(0)
 
-# training part
 
-samples = np.loadtxt('trainsamples.data', np.float32)
-responses = np.loadtxt('trainresponses.data', np.float32)
-responses = responses.reshape((responses.size, 1))
+def train(samples, responses):
+    samples = np.loadtxt(samples, np.float32)
+    responses = np.loadtxt(responses, np.float32)
+    responses = responses.reshape((responses.size, 1))
+    model = cv2.ml.KNearest_create()
+    model.train(samples, cv2.ml.ROW_SAMPLE, responses)
+    return model
 
-model = cv2.ml.KNearest_create()
-model.train(samples, cv2.ml.ROW_SAMPLE, responses)
 
-# testing part
+def get_solution(image):
+    im = cv2.imread(image)
+    out = np.zeros(im.shape, np.uint8)
 
-if len(sys.argv) < 2:
-    print("usage: python solver.py imageOfSudoku.png")
-    exit(-1)
+    raster_size = math.ceil((len(out) // 9)) + 2
+    field_size = len(out)
 
-# TODO size to 430x430
-im = cv2.imread(sys.argv[1])
-out = np.zeros(im.shape, np.uint8)
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 5, 2)
 
-raster_size = math.ceil((len(out) // 9)) + 2
-field_size = len(out)
+    # finding Contours
+    _, contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
-thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 5, 2)
-
-# finding Contours
-_, contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-
-row = []
-field = []
-_, y_old, _, _ = cv2.boundingRect(contours[0])
-for cnt in contours:
+    row = []
+    field = []
+    _, y_old, _, _ = cv2.boundingRect(contours[0])
+    for cnt in contours:
         [x, y, w, h] = cv2.boundingRect(cnt)
 
         area = w * h
@@ -103,34 +104,40 @@ for cnt in contours:
             cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 2)
             cv2.putText(out, string, (x, y + h), 0, 1, (0, 255, 0))
 
-row.sort(key=lambda x: int(x[1]))
-field.append(row)
-field = field[::-1]
+    row.sort(key=lambda x: int(x[1]))
+    field.append(row)
+    field = field[::-1]
 
-# compute number of zeros
-padded_field = []
-for row in field:
-    ext_row = [['0', 0, 0]] + row + [['0', field_size, 0]]
-    for i in range(0, len(ext_row) - 1):
-        distance = ext_row[i + 1][1] - ext_row[i][1]
-        if distance > 1.25 * raster_size:
-            padded_field.append([distance // raster_size, '0'])
-        if i != len(ext_row) - 2:
-            padded_field.append([1, ext_row[i + 1][0]])
+    # compute number of zeros
+    padded_field = []
+    for row in field:
+        ext_row = [['0', 0, 0]] + row + [['0', field_size, 0]]
+        for i in range(0, len(ext_row) - 1):
+            distance = ext_row[i + 1][1] - ext_row[i][1]
+            if distance > 1.25 * raster_size:
+                padded_field.append([distance // raster_size, '0'])
+            if i != len(ext_row) - 2:
+                padded_field.append([1, ext_row[i + 1][0]])
 
-# create game board
-A = [[] for i in range(0, 9)]
-i = k = 0
-for item in padded_field:
-    if i == 9:
-        k = k + 1
-        i = 0
-    for j in range(int(item[0])):
-        A[k].append(int(item[1]))
-        i = i + 1
+    # create game board
+    A = [[] for i in range(0, 9)]
+    i = k = 0
+    for item in padded_field:
+        if i == 9:
+            k = k + 1
+            i = 0
+        for j in range(int(item[0])):
+            A[k].append(int(item[1]))
+            i = i + 1
 
-# solve sudoku
-solve(A)
+    # solve sudoku
+    return solve(A)
 
-# cv2.imshow('out', out)
-# cv2.waitKey(0)
+
+# main
+
+model = train('trainsamples.data', 'trainresponses.data')
+solution = get_solution(sys.argv[1])
+for row in solution:
+    print(row)
+generate_image(solution)
